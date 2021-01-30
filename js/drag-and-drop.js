d8e9d2858d8414ecc1e54f64a2d114cef7814f76
YummyUranium <tbzphoenix@gmail.com>
2021-01-30 23:02:10 +0000

    js and css finished before testing

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -6,7 +6,37 @@ const dragEnd = event => {
     event.currentTarget.classList.remove('dragging');
 };
 
+const drag = event => {
+    event.dataTransfer.setData('text/html', event.currentTarget.outerHTML);
+};
+
 Array.from(document.querySelectorAll('.tile')).forEach(tile => {
     tile.addEventListener('dragstart', dragStart);
     tile.addEventListener('dragend', dragEnd);
 });
+
+const dragEnter = event => {
+    event.currentTarget.classList.add('drop');
+};
+
+const dragLeave = event => {
+    event.currentTarget.classList.remove('drop');
+};
+
+Array.from(document.querySelectorAll('.column')).forEach(column => {
+    column.addEventListener('dragenter', dragEnter);
+    column.addEventListener('dragleave', dragLeave);
+});
+
+const drop = event => {
+    Array.from(document.querySelectorAll('.column'))
+         .forEach(column => column.classList.remove('drop'));
+
+    document.querySelector(`[data-id="${event.dataTransfer.getData('text/plain')}"]`).remove();
+
+    event.currentTarget.innerHTML = event.currentTarget.innerHTML + event.dataTransfer.getData('text/html');
+};
+
+const allowDrop = event => {
+    event.preventDefault();
+};
